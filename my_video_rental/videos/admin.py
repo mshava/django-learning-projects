dea56557c65dfc8cfdf6d60fd4ec78d5ce66613d
Sakhile Tembani - Rhino Africa <sakhile@rhinoafrica.headoffice.co.za>
2017-10-18 14:05:04 +0200

    created filters for admin views

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -6,6 +6,8 @@ class MovieAdmin(admin.ModelAdmin):
 	fields = ['release_year', 'title', 'length']
 	
 	search_fields = ['title', 'length']
+	
+	filter_list = ['rease_year', 'length', 'title']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, ModelAdmin)
