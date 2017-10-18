96e00f1d9b3bb0a0ce55b753b5a8163c82b47535
Sakhile Tembani - Rhino Africa <sakhile@rhinoafrica.headoffice.co.za>
2017-10-18 13:43:59 +0200

    added search functionality to the admin interface

--- my_video_rental/videos/admin.py
+++ my_video_rental/videos/admin.py
@@ -4,6 +4,8 @@ from . import models
 class MovieAdmin(admin.ModelAdmin):
 
 	fields = ['release_year', 'title', 'length']
+	
+	search_fields = ['title', 'length']
 
 admin.site.register(models.Customer)
 admin.site.register(models.Movie, ModelAdmin)
